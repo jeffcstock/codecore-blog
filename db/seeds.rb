141cb7b18a26d550f7d1dbc220bb8e8ba5bb9a4c
Jeff Stock <jeffcstock@gmail.com>
2016-11-06 14:04:23 -0800

    Changed seedfile for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do
+10.times do
   Tag.create!(name: Faker::Book.genre)
 end
 
